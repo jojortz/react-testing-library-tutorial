7f6ba80f2dc301a34f76b51d8b7e829c51657d5a
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-03-08 13:19:02 -0800

    complete the LamaDev tutorial

--- client/src/components/LamaDev/Login.jsx
+++ client/src/components/LamaDev/Login.jsx
@@ -1,10 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const host_url = "https://jsonplaceholder.typicode.com/users/1"
+
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUsername(user.name);
+  }, [user])
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(host_url).then((res) => {
+      setUser(res.data);
+      setLoading(false);
+    })
+      .catch((err) => {
+        setError(true);
+      })
+  };
   return (
     <div>
+      <h2>{user.name}</h2>
       <form>
-        <input type="text"/>
-        <input type="password"/>
-        <button>Login</button>
+        <input type="text" placeholder="username" value={username}
+          onChange={(e) => {
+            e.preventDefault();
+            setUsername(e.target.value);
+          }}
+        />
+        <input type="password" placeholder="password" value={password}
+          onChange={(e) => {
+            e.preventDefault();
+            setPassword(e.target.value);
+          }} Í />
+        <button disabled={!username || !password} onClick={handleClick}>
+          {loading ? "Loading..." : "Login"}
+        </button>
+        <span data-testid="error" style={{ visibility: error ? "visible" : "hidden" }}>Something went wrong!</span>
       </form>
     </div>
   )
--- client/test/AppLama.test.js
+++ client/test/AppLama.test.js
@@ -1,6 +1,40 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from '../src/components/LamaDev/App';
+import Login from '../src/components/LamaDev/Login';
+
+const userData = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+};
+
+jest.mock("axios", () => ({
+  __esModule: true,
+  default: {
+    get: () => Promise.resolve({
+      data: userData,
+    }),
+  },
+}));
 
 describe("Lama Dev App - App", () => {
   test('renders lear react link', () => {
@@ -26,5 +60,110 @@ describe("Lama Dev App - App", () => {
   });
 });
 describe("Lama Dev - Login", () => {
+  test("username input should be rendered", () => {
+    render(<Login />);
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    expect(userInputEl).toBeInTheDocument()
+  })
+  test("password input should be rendered", () => {
+    render(<Login />);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputEl).toBeInTheDocument()
+  })
+  test("button input should be rendered", () => {
+    render(<Login />);
+    const buttonInputEl = screen.getByRole("button");
+    expect(buttonInputEl).toBeInTheDocument()
+  })
+  test("username input should be empty", () => {
+    render(<Login />);
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    expect(userInputEl.value).toBe("")
+  })
+  test("password input should be empty", () => {
+    render(<Login />);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputEl.value).toBe("")
+  })
+  test("button should be disabled if form not filled", () => {
+    render(<Login />);
+    const buttonInputEl = screen.getByRole("button");
+    expect(buttonInputEl).toBeDisabled();
+  })
+  test("Loading should not be rendered on button", () => {
+    render(<Login />);
+    const buttonInputEl = screen.getByRole("button");
+    expect(buttonInputEl).not.toHaveTextContent(/loading.../i);
+  })
+  test("error message should not be visible", () => {
+    render(<Login />);
+    const errorEl = screen.getByTestId("error");
+    expect(errorEl).not.toBeVisible();
+  })
+  test("username input should change", () => {
+    render(<Login />);
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    const testValue = "test";
+    fireEvent.change(userInputEl, { target: { value: testValue } });
+    expect(userInputEl.value).toBe(testValue);
+  })
+  test("password input should change", () => {
+    render(<Login />);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const testValue = "test";
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    expect(passwordInputEl.value).toBe(testValue);
+  })
 
+  test("button should be enabled if form is filled", () => {
+    render(<Login />);
+    const testValue = "test";
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    fireEvent.change(userInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    const buttonInputEl = screen.getByRole("button");
+    expect(buttonInputEl).not.toBeDisabled();
+  })
+  test("Loading should be rendered on button when clicked", () => {
+    render(<Login />);
+    const testValue = "test";
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const buttonInputEl = screen.getByRole("button");
+
+    fireEvent.change(userInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    fireEvent.click(buttonInputEl);
+
+    expect(buttonInputEl).toHaveTextContent(/loading.../i);
+  })
+  test("Loading should be not rendered on button after fetching", async () => {
+    render(<Login />);
+    const testValue = "test";
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const buttonInputEl = screen.getByRole("button");
+
+    fireEvent.change(userInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    fireEvent.click(buttonInputEl);
+
+    await waitFor(() => expect(buttonInputEl).not.toHaveTextContent(/loading.../i)
+    );
+})
+  test("User should be rendered on button after fetching", async () => {
+    render(<Login />);
+    const testValue = "test";
+    const userInputEl = screen.getByPlaceholderText(/username/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+    const buttonInputEl = screen.getByRole("button");
+
+    fireEvent.change(userInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    fireEvent.click(buttonInputEl);
+
+    const usernameHeading = await screen.findByText(userData.name);
+    expect(usernameHeading).toBeInTheDocument();
+})
 });
